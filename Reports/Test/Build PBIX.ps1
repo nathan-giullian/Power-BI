3f079eac9b0ace1506384b517044171e386da769
Nathan Giullian <nathan@giullian.com>
2023-05-04 22:29:26 -0600

    Created code from PBIX file

--- Reports/Test/Build PBIX.ps1
+++ Reports/Test/Build PBIX.ps1
@@ -1,13 +1,13 @@
 Write-host "Are you sure you want to overwrite the current PBIProj files in this folder? "
 $Result = Read-Host -Prompt "[Y/N]"
 if ($Result -eq 'Y') {
-	$ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
-	$subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Dson
-	foreach ($Report in $subManifestFile.Deployments.PBIReports) {
-		$PBIXFilePath = ".\Reports\Sales Reports\"
-		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)"
+	# $ReportsManifestPath = '.\Reports\Test\DeploymentManifest.json'
+	# $subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Dson
+	# foreach ($Report in $subManifestFile.Deployments.PBIReports) {
+		$PBIXFilePath = ".\Reports\Test\"
+		$BPIProjPath = ".\Reports\Test\Test File - NGG"
 		Write-host "Expanding PBIX File: $BPIProjPath to $PBIXFilePath"
 
 		.\Util\PBIToolsCore\pbi-tools.core.exe compile $BPIProjPath -outPath $PBIXFilePath -format PBIX -overwrite True
 	}
-}
+# }
--- Reports/Test/Extract to pbiProj.ps1
+++ Reports/Test/Extract to pbiProj.ps1
@@ -1,14 +1,14 @@
 Write-host "Are you sure you want to overwrite the current PBTProj files in this folder? "
-$Result = Read-Host -Prompt "[Y/N]" 
+#$Result = Read-Host -Prompt "[Y/N]" 
 if ($Result -eq 'Y') {
-	$ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
-	$subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Json 
-	foreach ($Report in $subManifestFile.Deployments.PBIReports) {
-		$PBIXFilePath = ".\Reports\Sales Reports\$($Report.PBIXFileName)"
-		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)"
+	#$ReportsManifestPath = '.\Reports\Test\DeploymentManifest.json'
+	#$subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Json 
+	#foreach ($Report in $subManifestFile.Deployments.PBIReports) {
+		$PBIXFilePath = ".\Reports\Test\Test File - NGG.pbix"
+		$BPIProjPath = ".\Reports\Test\Test File - NGG"
 		Write-host "Extrcting PB1X File: $BPIProjPath to $PBIXFilePath"
 		
 		.\Util\PBITools\pbi-tools.exe extract "$PBIXFilePath" -extractFolder "$BPIProjPath" -modelSerialization Default
-	}
+	#}
 }
 
