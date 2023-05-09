49fa9d593b5e997fb000177d697524c5a0c1b431
Nathan Giullian <nathan@giullian.com>
2023-05-08 22:05:44 -0600

    Updating code for client

--- Reports/Build PBIX.ps1
+++ Reports/Build PBIX.ps1
@@ -1,13 +1,13 @@
 Write-host "Are you sure you want to overwrite the current PBIProj files in this folder? "
 $Result = Read-Host -Prompt "[Y/N]"
 if ($Result -eq 'Y') {
-	$ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
-	$subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Dson
-	foreach ($Report in $subManifestFile.Deployments.PBIReports) {
-		$PBIXFilePath = ".\Reports\Sales Reports\"
-		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)"
+	# $ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
+	# $subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Dson
+	# foreach ($Report in $subManifestFile.Deployments.PBIReports) {
+		$PBIXFilePath = ".\Reports\Sales Reports\" #Add your file path here to the specific report folder
+		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)" #Add the file path with the folder for the json and other files here
 		Write-host "Expanding PBIX File: $BPIProjPath to $PBIXFilePath"
 
 		.\Util\PBIToolsCore\pbi-tools.core.exe compile $BPIProjPath -outPath $PBIXFilePath -format PBIX -overwrite True
 	}
-}
+# }
--- Reports/Extract to pbiProj.ps1
+++ Reports/Extract to pbiProj.ps1
@@ -1,14 +1,14 @@
 Write-host "Are you sure you want to overwrite the current PBTProj files in this folder? "
 $Result = Read-Host -Prompt "[Y/N]" 
 if ($Result -eq 'Y') {
-	$ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
-	$subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Json 
-	foreach ($Report in $subManifestFile.Deployments.PBIReports) {
-		$PBIXFilePath = ".\Reports\Sales Reports\$($Report.PBIXFileName)"
-		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)"
+	# $ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
+	# $subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Json 
+	# foreach ($Report in $subManifestFile.Deployments.PBIReports) {
+		$PBIXFilePath = ".\Reports\Sales Reports\$($Report.PBIXFileName)" #This is the name of the PBIX file in the specific report folder
+		$BPIProjPath = ".\Reports\Sales Reports\$($Report.FolderPath)" #This is the folder that contains the json and other files
 		Write-host "Extrcting PB1X File: $BPIProjPath to $PBIXFilePath"
 		
 		.\Util\PBITools\pbi-tools.exe extract "$PBIXFilePath" -extractFolder "$BPIProjPath" -modelSerialization Default
 	}
-}
+# }
 
