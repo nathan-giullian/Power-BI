dd3577f185a0d1912e9d12747e0bb3c7c12e6a00
Nathan Giullian <nathan@giullian.com>
2023-06-07 14:06:00 -0600

    Syncing changes

--- Reports/Shipley/Extract to pbiProj.ps1
+++ Reports/Shipley/Extract to pbiProj.ps1
@@ -4,7 +4,7 @@ if ($Result -eq 'Y') {
 	# $ReportsManifestPath = '.\Reports\Sales Reports\DeploymentManifest.json'
 	# $subManifestFile = Get-Content -Path $ReportsManifestPath | ConvertFrom-Json 
 	# foreach ($Report in $subManifestFile.Deployments.PBIReports) {
-		$PBIXFilePath = ".\Reports\Shipley\Pipeline Dataset.pbix" #This is the name of the PBIX file in the specific report folder
+		$PBIXFilePath = ".\Reports\Shipley\Pipeline Dataset-NGG-DESKTOP.pbix" #This is the name of the PBIX file in the specific report folder
 		$BPIProjPath = ".\Reports\Shipley\Pipeline Dataset" #This is the folder that contains the json and other files
 		Write-host "Extrcting PB1X File: $BPIProjPath to $PBIXFilePath"
 		
